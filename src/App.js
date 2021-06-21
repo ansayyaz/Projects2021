fef59b89aa053d96fd0cb4ee600226e8e76f739a
M,Ans Ayyaz <ansayyaz1@gmail.com>
2021-06-22 02:50:31 +0500

    sumary (requird)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Hello Ans  </h1> 
+ <pr>
+ A paragraph is a series of related sentences developing a central idea, called the topic.
+  Try to think about paragraphs in terms of thematic unity:
+ a paragraph is a sentence or a group of sentences that supports one central,
+   unified idea. Paragraphs add one idea at a time to your broader argument.
+ </pr>
+    </div>
+  );
+}
+
+export default App;
